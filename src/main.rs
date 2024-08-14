62a65804637aaac379f146e645d13dde554a14d4
bruce.li <127000969+binary-bruce@users.noreply.github.com>
2024-08-14 21:35:39 +0800

    feat: refactor (#1)
    * breakdown
    
    * fix

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,15 @@
 use std::collections::HashMap;
 use std::io;
-use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
-use std::os::unix::io::{AsRawFd, RawFd};
+use std::net::TcpListener;
+use std::os::fd::AsRawFd;
 
-#[allow(unused_macros)]
-macro_rules! syscall {
-    ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
-        let res = unsafe { libc::$fn($($arg, )*) };
-        if res == -1 {
-            Err(std::io::Error::last_os_error())
-        } else {
-            Ok(res)
-        }
-    }};
-}
+use epoll::*;
+use request_context::RequestContext;
 
-#[derive(Debug)]
-pub struct RequestContext {
-    pub stream: TcpStream,
-    pub content_length: usize,
-    pub buf: Vec<u8>,
-}
-
-impl RequestContext {
-    fn new(stream: TcpStream) -> Self {
-        Self {
-            stream,
-            buf: Vec::new(),
-            content_length: 0,
-        }
-    }
-
-    fn read_cb(&mut self, key: u64, epoll_fd: RawFd) -> io::Result<()> {
-        let mut buf = [0u8; 4096];
-        match self.stream.read(&mut buf) {
-            Ok(_) => {
-                if let Ok(data) = std::str::from_utf8(&buf) {
-                    self.parse_and_set_content_length(data);
-                }
-            }
-            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
-            Err(e) => {
-                return Err(e);
-            }
-        };
-        self.buf.extend_from_slice(&buf);
-        if self.buf.len() >= self.content_length {
-            println!("got all data: {} bytes", self.buf.len());
-            modify_interest(epoll_fd, self.stream.as_raw_fd(), listener_write_event(key))?;
-        } else {
-            modify_interest(epoll_fd, self.stream.as_raw_fd(), listener_read_event(key))?;
-        }
-        Ok(())
-    }
-
-    fn parse_and_set_content_length(&mut self, data: &str) {
-        if data.contains("HTTP") {
-            if let Some(content_length) = data
-                .lines()
-                .find(|l| l.to_lowercase().starts_with("content-length: "))
-            {
-                if let Some(len) = content_length
-                    .to_lowercase()
-                    .strip_prefix("content-length: ")
-                {
-                    self.content_length = len.parse::<usize>().expect("content-length is valid");
-                    println!("set content length: {} bytes", self.content_length);
-                }
-            }
-        }
-    }
-
-    fn write_cb(&mut self, key: u64, epoll_fd: RawFd) -> io::Result<()> {
-        match self.stream.write(HTTP_RESP) {
-            Ok(_) => println!("answered from request {}", key),
-            Err(e) => eprintln!("could not answer to request {}, {}", key, e),
-        };
-        self.stream.shutdown(std::net::Shutdown::Both)?;
-        let fd = self.stream.as_raw_fd();
-        remove_interest(epoll_fd, fd)?;
-        close(fd);
-        Ok(())
-    }
-}
-
-const READ_FLAGS: i32 = libc::EPOLLONESHOT | libc::EPOLLIN;
-const WRITE_FLAGS: i32 = libc::EPOLLONESHOT | libc::EPOLLOUT;
-
-const HTTP_RESP: &[u8] = br#"HTTP/1.1 200 OK
-content-type: text/html
-content-length: 5
-
-Hello"#;
+#[macro_use]
+mod syscall;
+mod epoll;
+mod request_context;
 
 fn main() -> io::Result<()> {
     let mut request_contexts: HashMap<u64, RequestContext> = HashMap::new();
@@ -159,51 +76,4 @@ fn main() -> io::Result<()> {
             }
         }
     }
-}
-
-fn epoll_create() -> io::Result<RawFd> {
-    let fd = syscall!(epoll_create1(0))?;
-    if let Ok(flags) = syscall!(fcntl(fd, libc::F_GETFD)) {
-        let _ = syscall!(fcntl(fd, libc::F_SETFD, flags | libc::FD_CLOEXEC));
-    }
-
-    Ok(fd)
-}
-
-fn listener_read_event(key: u64) -> libc::epoll_event {
-    libc::epoll_event {
-        events: READ_FLAGS as u32,
-        u64: key,
-    }
-}
-
-fn listener_write_event(key: u64) -> libc::epoll_event {
-    libc::epoll_event {
-        events: WRITE_FLAGS as u32,
-        u64: key,
-    }
-}
-
-fn close(fd: RawFd) {
-    let _ = syscall!(close(fd));
-}
-
-fn add_interest(epoll_fd: RawFd, fd: RawFd, mut event: libc::epoll_event) -> io::Result<()> {
-    syscall!(epoll_ctl(epoll_fd, libc::EPOLL_CTL_ADD, fd, &mut event))?;
-    Ok(())
-}
-
-fn modify_interest(epoll_fd: RawFd, fd: RawFd, mut event: libc::epoll_event) -> io::Result<()> {
-    syscall!(epoll_ctl(epoll_fd, libc::EPOLL_CTL_MOD, fd, &mut event))?;
-    Ok(())
-}
-
-fn remove_interest(epoll_fd: RawFd, fd: RawFd) -> io::Result<()> {
-    syscall!(epoll_ctl(
-        epoll_fd,
-        libc::EPOLL_CTL_DEL,
-        fd,
-        std::ptr::null_mut()
-    ))?;
-    Ok(())
 }
